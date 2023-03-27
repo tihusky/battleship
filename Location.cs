c71fd612a7ca368916166ff4cd68aea135c93412
Mirko Förster <mfoerster.hb@gmail.com>
2023-03-27 13:50:30 +0200

    Implement the Location struct

---
+++ Location.cs
@@ -0,0 +1,28 @@
+internal struct Location
+{
+    public int Row { get; init; }
+    public int Column { get; init; }
+
+    public static Location FromString(string location)
+    {
+        int row = location[0] switch
+        {
+            'A' => 0,
+            'B' => 1,
+            'C' => 2,
+            'D' => 3,
+            'E' => 4,
+            'F' => 5,
+            'G' => 6,
+            'H' => 7,
+            'I' => 8,
+            'J' => 9,
+            _ => Int32.MaxValue
+        };
+
+        // Make the location zero-based (e.g. "A1" would be (0, 0))
+        int col = Convert.ToInt32(location.Substring(1)) - 1;
+
+        return new Location { Row = row, Column = col };
+    }
+}
